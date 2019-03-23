347711f481dd5b655f508c2ca8d0e6c30d4d3d8c
Justin Mitchel <hello@teamcfe.com>
2019-03-23 11:47:21 -0700

    7 - Why you should use React

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+function App () {
+  
+  const [value, setValue] = useState(1)
+
+ 
+  function addOne(){
+    console.log('working...')
+    setValue(value + 1)
   }
+  return <div>
+        <h1>Hello World {value}</h1>
+        <button onClick={addOne}>Add 1</button>
+    </div>
 }
 
 export default App;

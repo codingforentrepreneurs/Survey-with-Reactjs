7e36ebd06eb35cf07ed2ed9e03ed41d90e4ab1be
CFE <hello@teamcfe.com>
2019-03-25 15:47:46 -0700

    20 - Create a Form Input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+const SurveyInput = props => {
+    const [name, setName] = useState('')
+    const [inputType, setType] = useState(props.type ? props.type : 'text')
+    const handleChange = event => {
+        setName(event.target.value)
+    }
+
+    return  <input 
+        onChange={handleChange}
+        value={name}
+        type={inputType} 
+        placeholder='Your Name'
+        name='full_name' />
+}
+
+
 
 const App = props => {
     return <div className='col-10 mx-auto text-center'>
         <h1>Hello There</h1>
+        <SurveyInput />
+
+        <SurveyInput type='number' />
+
+
+        <SurveyInput type='email' />
     </div>
 }
 

dcbc6dd11630b79eccb6892f7383b464dbc8ff28
CFE <hello@teamcfe.com>
2019-03-27 15:32:19 -0700

    Final Example

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = props => {
 
     return <div className='col-6 mx-auto text-center'>
         <h1 className='mb-5'>Hello There</h1>
-        <Survey surveyId={1} />
+        <Survey surveyId={3} />
     </div>
 }
 
